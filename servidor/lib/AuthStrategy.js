6737b853162a0296e17b3b5f7a76b0763a81e0e1
PonfeESP <128382547+PonfeESP@users.noreply.github.com>
2023-05-18 17:56:07 +0200

    f

--- servidor/lib/AuthStrategy.js
+++ servidor/lib/AuthStrategy.js
@@ -1,15 +1,16 @@
 import { Strategy as LocalStrategy } from 'passport-local';
 import Cliente from '../models/Cliente.model.js';
 import EmpresaPromotora from '../models/Empresa.model.js';
+import Admin from '../models/Admin.model.js';
 
 export const strategyInit = passport => {
   passport.use('local-cliente', new LocalStrategy({
     usernameField: 'email',
-    passwordField: 'contraseña'
-  }, (email, contraseña, done) => {
+    passwordField: 'password'
+  }, (email, password, done) => {
     Cliente.query().findOne({ email: email }).then(cliente => {
       if (!cliente) return done(null, false, { error: 'Cliente desconocido' });
-      cliente.verifyContraseña(String(contraseña), (err, contraseñaEsCorrecta) => {
+      cliente.verifyPassword(String(password), (err, contraseñaEsCorrecta) => {
         if (!!err) return done(err);
         if (!contraseñaEsCorrecta) return done(null, false);
         return done(null, cliente);
@@ -21,11 +22,11 @@ export const strategyInit = passport => {
 
   passport.use('local-empresa', new LocalStrategy({
     usernameField: 'email',
-    passwordField: 'contraseña'
-  }, (email, contraseña, done) => {
+    passwordField: 'password'
+  }, (email, password, done) => {
     EmpresaPromotora.query().findOne({ email: email }).then(empresa => {
       if (!empresa) return done(null, false, { error: 'Empresa desconocida' });
-      empresa.verifyContraseña(String(contraseña), (err, contraseñaEsCorrecta) => {
+      empresa.verifyPassword(String(password), (err, contraseñaEsCorrecta) => {
         if (!!err) return done(err);
         if (!contraseñaEsCorrecta) return done(null, false);
         return done(null, empresa);
@@ -35,25 +36,39 @@ export const strategyInit = passport => {
     });
   }));
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    Promise.all([
-      Cliente.query().findById(id),
-      EmpresaPromotora.query().findById(id)
-    ]).then(([cliente, empresa]) => {
-      if (cliente) {
-        done(null, cliente);
-      } else if (empresa) {
-        done(null, empresa);
-      } else {
-        done(null, null);
-      }
+  passport.use('local-administrador', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  }, (email, password, done) => {
+    Admin.query().findOne({ email: email }).then(administrador => {
+      if (!administrador) return done(null, false, { error: 'Administrador desconocido' });
+      administrador.verifyPassword(String(password), (err, contraseñaEsCorrecta) => {
+        if (!!err) return done(err);
+        if (!contraseñaEsCorrecta) return done(null, false);
+        return done(null, administrador);
+      });
     }).catch(err => {
       done(err);
     });
+  }));
+
+  passport.serializeUser((user, done) => {
+    const userType = user instanceof Cliente ? 'cliente' : user instanceof EmpresaPromotora ? 'empresa' : user instanceof Admin ? 'admin' : undefined
+    done(null, {
+      email: user.email,
+      userType
+    })
+  });
+
+  passport.deserializeUser((user, done) => {
+    const dbQuery = user.userType === 'cliente'
+      ? Cliente.query().findById(user.email)
+      : user.userType === 'admin'
+        ? Admin.query().findById(user.email)
+        : null;
+    if (!!dbQuery){
+      dbQuery.then(res => done(null, res))
+    } else done (null, null);
   });
 };
 
--- servidor/models/Admin.model.js
+++ servidor/models/Admin.model.js
@@ -1,32 +1,27 @@
 import { Model } from 'objection';
 import bcrypt from 'bcryptjs';
 
-export default class Cliente extends Model {
-  static tableName = 'clientes';
+export default class Admin extends Model {
+  static tableName = 'admin';
 
   static idColumn = 'id';
 
   static jsonSchema = {
     type: 'object',
-    required: ['email', 'nombre', 'apellidos', 'dni', 'telefono', 'fechanacimiento'],
+    required: ['email'],
 
     properties: {
       id: { type: 'integer' },
-      nombre: { type: 'string' },
-      apellidos: { type: 'string' },
       email: { type: 'string' },
       password: { type: 'string' },
-      dni: { type: 'string' },
-      telefono: { type: 'string' },
-      fechanacimiento: { type: 'string' },
-    }
+    },
   };
 
   set unsecurePassword(unsecurePassword) {
     this.password = bcrypt.hashSync(unsecurePassword, bcrypt.genSaltSync(10));
-  };
+  }
 
   verifyPassword(unsecurePassword, callback) {
     return bcrypt.compare(String(unsecurePassword), String(this.password), callback);
-  };
+  }
 }
