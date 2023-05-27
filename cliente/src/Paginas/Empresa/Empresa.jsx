7a5d116e9b87f0143e8a9ee67773585a9fd673b2
PonfeESP <128382547+PonfeESP@users.noreply.github.com>
2023-05-27 18:39:56 +0200

    as

---
+++ cliente/src/Paginas/Empresa/Empresa.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// Importación de Axios
+import axios from 'axios';
+
+// Importaciones de Material UI
+import { Paper, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+
+
+// Importaciones de Componentes
+import {ClientePag} from './Componentes/EmpresaPag';
+import { axiosConfig } from '../../constant/axiosConfig.constant';
+
+export const Cliente = () => {
+  const [userData, setUserData] = useState();
+  const [logoutError, setLogoutError] = useState();
+
+  const navigate = useNavigate();
+
+    useEffect(() => {
+      document.title = "EMPRESA";
+    }, []);
+
+    useEffect(() => { // Obtener User
+      axios({
+          url: 'http://localhost:8000/user',
+          method: 'GET',
+          withCredentials: true,
+          //timeout: 5000,
+          //signal: AbortSignal.timeout(5000) //Aborts request after 5 seconds
+      })
+      .then(res => {
+        setUserData(res.data);
+      })
+      .catch(err => console.log(err))
+  }, []);
+
+  const performLogout = (event) => {
+    event.preventDefault();
+    setLogoutError('');
+
+    if(!!userData){
+        axios({
+          ...axiosConfig,
+            url: 'http://localhost:8000/logout',
+            method: 'POST',
+            //timeout: 5000,
+            //signal: AbortSignal.timeout(5000) //Aborts request after 5 seconds
+        }).then((response) =>{
+            if(response.data.status === 'Ok')
+                navigate('/'); // Navega a la página de Inicio
+            else
+                setLogoutError(response.data.error);
+        })
+        .catch((error) => {
+            console.log('Error en el cierre de sesión');
+            setLogoutError('Error en el Cierre de Sesión. Inténtelo más tarde.');
+        })
+    }
+};
+  
+    return (
+      <div>
+        <Paper>
+          <Typography variant="h4" color="primary">EMPRESA</Typography>
+          <Button onClick={e => performLogout(e)}>CERRAR SESION</Button>
+          <Typography>{!!userData && userData.id}</Typography>
+          <Typography>{!!userData && userData.userType}</Typography>
+          <ClientePag />
+        </Paper>
+      </div>
+    );
+  };
+  
+  export default Cliente;
